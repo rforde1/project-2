a2c45216a874350993499c1664ce00ba0f33249c
ramiforde <ramiforde@gmail.com>
2020-03-10 20:18:22 -0400

    Fixed bugs

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-
+ 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
