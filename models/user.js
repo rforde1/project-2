ab4a977030c2ab48ac083e404d396e5664f12507
Garrett Reichman <gsreichman@gmail.com>
2020-03-12 10:59:56 -0400

    added sequelize models and got lint rules to work

--- models/user.js
+++ models/user.js
@@ -25,18 +25,21 @@ module.exports = function(sequelize, DataTypes) {
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
   User.addHook("beforeCreate", function(user) {
-    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
   });
 
-  User.associate = function(models){
-    User.hasMany(models.Post,{
-      onDelete:"cascade"
+  User.associate = function(models) {
+    User.hasMany(models.Post, {
+      onDelete: "cascade"
     });
-    User.hasMany(models.Reply,{
-      onDelete:"cascade"
+    User.hasMany(models.Reply, {
+      onDelete: "cascade"
     });
-  }
-
+  };
 
   return User;
 };
