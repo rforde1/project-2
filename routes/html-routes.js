d6cac38f06ccb1d685d6818a47ad00b12f7d2a2c
Garrett Reichman <gsreichman@gmail.com>
2020-03-14 11:38:08 -0400

    added handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
+var db = require("../models");
 
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -24,7 +25,20 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    // res.sendFile(path.join(__dirname, "../public/members.html"));
+    db.User.findOne(
+      {
+        include: [db.Post]
+      },
+      {
+        where: {
+          id: req.user.id
+        }
+      }
+    ).then(results => {
+      console.log(results);
+      res.render("profile", results);
+    });
   });
 
   app.get("/home", function(req, res) {
