e920dd514e39d92d90f7add76d73b1864bf40733
Garrett Reichman <gsreichman@gmail.com>
2020-03-19 18:50:40 -0400

    fixed signup.html

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
   // Getting references to our form and input
-  var signUpForm = $("form.signup");
+  //var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
-
   // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", function(event) {
+  $("#signup").on("click", function(event) {
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
-
     if (!userData.email || !userData.password) {
       return;
     }
@@ -20,7 +18,6 @@ $(document).ready(function() {
     emailInput.val("");
     passwordInput.val("");
   });
-
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
   function signUpUser(email, password) {
